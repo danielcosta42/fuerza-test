ea5755cbc6ac4f2ac81761d619ae256cf5307359
Daniel Costa <daniel.cfdutra13@gmail.com>
2021-12-21 17:54:42 -0300

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -6,6 +6,15 @@ import cors from 'cors';
 module.exports = () => {
   const app = express();
 
+  app.use(function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
+  
   app.use(express.static(pathToSwaggerUi))
 
   app.use(express.json());
